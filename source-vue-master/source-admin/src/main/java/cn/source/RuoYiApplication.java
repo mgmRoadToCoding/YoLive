dbfaf312c981c224291fbb4fde28030059b47cd0
mgm <114996118+mgmRoadToCoding@users.noreply.github.com>
2025-03-29 03:54:54 +0900

    111

--- source-vue-master/source-admin/src/main/java/cn/source/RuoYiApplication.java
+++ source-vue-master/source-admin/src/main/java/cn/source/RuoYiApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * 启动程序
  *
@@ -14,6 +18,13 @@ public class RuoYiApplication
 {
     public static void main(String[] args)
     {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/source_vue", "root", "wojiaoldh520");
+            System.out.println("数据库连接成功！");
+        } catch (SQLException e) {
+            System.err.println("数据库连接失败：" + e.getMessage());
+        }
+
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  开源字节(https://sourcebyte.vip)  ლ(´ڡ`ლ)ﾞ  \n" +
